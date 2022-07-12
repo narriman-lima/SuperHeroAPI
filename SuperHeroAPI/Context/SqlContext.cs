57bccf437cbe88e8e9ff6b9920f7a2a253010c52
Narriman Lima <narrimancslima@gmail.com>
2022-07-12 18:28:21 -0300

    Initial commit

---
+++ SuperHeroAPI/Context/SqlContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SuperHeroAPI.Models;
+
+namespace SuperHeroAPI.Context
+{
+    public class SqlContext : DbContext
+    {
+        public SqlContext(DbContextOptions<SqlContext> options) : base(options) { }
+        public DbSet<SuperHero> SuperHeroes { get; set; }
+    }
+}
